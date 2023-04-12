84bcd63465563902ce9caac411a1f08d1de20f45
jin <jinhyuk1219@gmail.com>
2023-04-12 22:26:47 +0900

    chapter_8 complete

---
+++ techitJava/src/Operator.java
@@ -0,0 +1,94 @@
+public class Operator {
+    public static void main(String[] args) {
+        // 산술 연산자
+
+//        int a = 10;
+//        int b = 20;
+//        String c = "abc";
+//
+//        double d = 10;
+//        double e = 20;
+//        System.out.println(d / e);
+//
+//        System.out.println(a + b);
+//        System.out.println(a - b);
+//        System.out.println(a * b);
+//        System.out.println(a / b); // 정수 형태로 선언하면 분수값이 안나옴
+//        System.out.println(a % b);
+
+//        System.out.println(a + c); //문자열로 반환됨
+
+        // 비교연산자
+
+//        int a = 5;
+//        int b = 8;
+
+//        System.out.println(a > b);
+//        System.out.println(a < b);
+//        System.out.println(a <= b);
+//        System.out.println(a <= b);
+//
+//        // = : 대입연산자, ==: 비교연산자
+//        System.out.println(a == b);
+//        System.out.println(a != b); // 같지 않으면 true
+
+        // 논리 연산자
+
+//        int a = 3;
+//        int b = 3;
+//        int c = 5;
+//
+//        // AND(&&) 모든 조건 만족해야 true
+//        System.out.println(c > a && c > b);
+//
+//        // OR(||) 하나의 조건만 만족해도 true
+//        System.out.println(c > a || c < b);
+//
+//        //NOT(!)
+//        System.out.println(!true);
+//        System.out.println(!false);
+
+
+        // 대입 연산자
+
+        int a = 1;
+        double price = 12.4;
+
+        // 증감 연산자
+        System.out.println(a++); //값이 컴퓨터에 인자로 이미 던져진 이후에 1을 더해줌 즉 출력을 수행하고 1을 더해줌
+        System.out.println(++a); //1을 먼저 더 해준뒤 컴퓨터에 인자로 던져준다. 즉 더해준 뒤 출력을 수행함
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
